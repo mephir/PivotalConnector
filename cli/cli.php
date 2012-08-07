3331b9469799e90f86be680692111d48053b6f73
Pawel Wilk <pawel@wilk.pro>
2012-08-07 10:41:56 +0200

    autoload

---
+++ cli/cli.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * This file is part of the PivotalConnector package.
+ * Pawel Wilk <pwilkmielno@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once('cli/cliAutoload.class.php');
+cliAutoload::getInstance()->register();
