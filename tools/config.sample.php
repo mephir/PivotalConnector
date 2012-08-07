2f8ae7bf45565f4ac4962a354cd3d180a0673625
Pawel Wilk <pawel@wilk.pro>
2012-08-07 14:30:36 +0200

    adding tools

---
+++ tools/config.sample.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the PivotalConnector package.
+ * Pawel Wilk <pwilkmielno@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+define('PIVOTAL_TOKEN', ''); //API token for pivotal access, token could be generated on https://www.pivotaltracker.com/profile
+define('SSL_ENABLED', true);
---
+++ tools/pivotal_rss.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the PivotalConnector package.
+ * Pawel Wilk <pwilkmielno@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This script is retrieving activities and return them in rss format
+ */
+
+include_once('config.php');
